bd32a6f8f379dd743f943fbf69b30bc2666f97da
Théo Teenster <thewro11@gmail.com>
2023-04-04 15:58:47 +0700

    add Database

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,12 @@
 module org.importpm {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
     requires transitive javafx.graphics;
 
     opens org.importpm to javafx.fxml;
     opens org.importpm.controllers to javafx.fxml;
     exports org.importpm;
     exports org.importpm.controllers.enums;
+    
 }
