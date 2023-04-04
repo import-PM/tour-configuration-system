05c2a4b09eb7f4a88291081cd65a4c7b5e99e7e0
Théo Teenster <thewro11@gmail.com>
2023-04-05 05:52:57 +0700

    add CheckPaymentPageController

--- src/main/java/org/importpm/models/enums/QuotationStatus.java
+++ src/main/java/org/importpm/models/enums/QuotationStatus.java
@@ -1,7 +1,24 @@
 package org.importpm.models.enums;
 
 public enum QuotationStatus {
-    UNPAID,
-    PAID,
-    CANCEL
+    // TODO: Images needed
+    UNPAID("ยังไม่จ่ายเงิน", ""),
+    PAID("จ่ายเงินแล้ว", ""),
+    CANCEL("ยกเลิก", "");
+
+    private String name;
+    private String imagePath;
+
+    QuotationStatus(String name, String imagePath) {
+        this.name = name;
+        this.imagePath = imagePath;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
 }
